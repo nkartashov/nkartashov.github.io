2772550efb84d863b6e192a8403d63b9e5229fa0
Nikita Kartashov <snailandmail@gmail.com>
2019-01-20 00:40:49 +0000

    Use correct dir in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SBASE_DIR="$SCRIPT_DIR/.."
-cd "$BASE_DIR" && git subtree push --prefix _site origin master
+cd "$SBASE_DIR" && git subtree push --prefix _site origin master "$@"
